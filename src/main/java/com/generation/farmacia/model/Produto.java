0c2e2bbc249e12f8b6b6932221508f752e534504
Veronica <vevef.santos@hotmail.com>
2025-03-19 10:49:34 -0300

    Crud do Produto

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,82 @@
+package com.generation.farmacia.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O nome tem que ser maior que 5 e menor que 1000!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 5, max = 100, message = "A descrição tem que ser maior que 5 e menor que 1000!")
+	private String descricao; 
+	
+	@NotBlank(message = "O atributo Data de Validade é obrigatório!")
+	@NotNull(message = "O Atributi Data de Validade é obrigatório")
+	private String dataValidade;
+	
+	@NotNull(message = "O Atributo Preço é obrigatório")
+	private float preco;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(String dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+	
+}
